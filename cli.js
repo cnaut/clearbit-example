7ac6f007f54f6f1d69358551625e9b6894cc69d6
Charles Naut <cpn585@gmail.com>
2020-05-29 21:14:10 -0400

    Handle both emails and domains

--- cli.js
+++ cli.js
@@ -1,14 +1,15 @@
 var clearbit = require('clearbit')('');
 
 var args  = process.argv.slice(2);
-var email = args[0];
+var arg = args[0];
 
-if (!email) {
-  console.error('Usage: cli.js [email]')
+if (!arg) {
+  console.error('Usage: cli.js [email|domain]')
   process.exit(1);
 }
 
-clearbit.Enrichment.find({email: email, stream: true})
+if (arg.includes("@")) {
+  clearbit.Enrichment.find({email: arg, stream: true})
   .then(function (lookup) {
     console.log(lookup);
   })
@@ -18,3 +19,16 @@ clearbit.Enrichment.find({email: email, stream: true})
   .catch(function (err) {
     console.error(err);
   });
+} else {
+  clearbit.Enrichment.find({domain: arg, stream: true})
+  .then(function (lookup) {
+    console.log(lookup);
+  })
+  .catch(clearbit.Enrichment.NotFoundError, function (err) {
+    console.log(err);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+}
+
